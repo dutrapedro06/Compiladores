9ee4e52e747796deab13b88bd10ad5f95d4775bb
Pedro Dutra <dutrapedro06@gmail.com>
2026-08-27 11:55:35 -0300

    Subindo a etapa 2

--- atividade 2/minilexer/minilexer.c
+++ atividade 2/minilexer/minilexer.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+//o enum nesse caso serve para enumerar os valores de cada token, para que seja mais fácil de identificar (Ex: 0 = token palavra reservada)
+//e definir quais são os tipos de tokens que existem e o lexer vai reconhecer.
+typedef enum { 
+    TOKEN_PALAVRA_RESERVADA,
+    TOKEN_IDENTIFICADOR,
+    TOKEN_NUMERO_INTEIRO,
+    TOKEN_NUMERO_REAL,
+    TOKEN_LITERAL_CARACTERE,
+    TOKEN_OPERADOR,
+    TOKEN_DELIMITADOR,
+    TOKEN_ERRO
+} TipoToken;
+
+typedef struct { //struct para agrupar as informações que queremos de cada token.
+    TipoToken tipo; //armazena o tipo do token, que é definido pelo enum TipoToken
+    char lexema[64];
+    int linha;
+    int coluna;
+} Token;
+
 int main(int argc, char *argv[]) { //argc quantidade de argumentos e o argv os argumentos passados
     FILE *arquivo;
 
